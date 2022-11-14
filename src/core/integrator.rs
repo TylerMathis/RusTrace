0ebfba3ac459650a8416835cc19c3555d00e0199
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-11-13 22:24:54 -0500

    Integrator intf

--- src/core/integrator.rs
+++ src/core/integrator.rs
@@ -1,8 +1,15 @@
+use crate::core::scene::Scene;
+
 /////////////////////
 // BEGIN INTERFACE //
 /////////////////////
 
-pub trait Integrator {}
+/// An [Integrator] is responsible for aggregating light information over a [Scene].
+/// Generally used to generate a two dimensional image, but is intentionally left general
+/// to support other use cases.
+pub trait Integrator {
+    fn render(scene: &Scene);
+}
 
 //////////////////////////
 // END INTERFACE        //
